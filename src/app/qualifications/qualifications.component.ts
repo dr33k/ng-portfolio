57d17be6c662a4fa5d52af760c25d245e125d56b
dr33k <dr33k@users.noreply.github.com>
2023-09-20 09:08:37 +0100

    Counter works

--- src/app/qualifications/qualifications.component.ts
+++ src/app/qualifications/qualifications.component.ts
@@ -1,27 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-
+import * as moment from 'moment';
 @Component({
   selector: 'app-qualifications',
-  templateUrl: './qualifications.component.html'
+  templateUrl: './qualifications.component.html',
+  styles: [`
+  .uppercase{
+    text-transform: uppercase
+  }
+  `]
 })
 export class QualificationsComponent implements OnInit {
 
-  counter?: string;
+  counter: any;
+  skills: any = {
+    java: ['spring boot', 'spring cloud', 'quarkus', 'hibernate orm', 'junit5', 'mockito', 'thymeleaf'],
+    cs: ['.net core 6', 'asp.net', 'entity framework', 'razor pages'],
+    ts: ['node js', 'express', 'angular', 'prisma orm', 'jest', 'supertest'],
+    sql: ['postgresql', 'sql server']
+  }
 
-  constructor(){}
+  constructor(){ this.counter=""}
 
   ngOnInit():void{
-    setInterval(this.update, 1000);
+    setInterval(()=>    this.counter = this.update()
+    , 1000);
   }
-  update():void{
-    const startDate = new Date("2021-01-01");
-    const difference = new Date().getMilliseconds() - startDate.getMilliseconds();
+  update():string{
+    const difference =moment.now().valueOf() - moment("2021-01-01").valueOf();
 
-    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
-    this.counter = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
-}
+    const days = Math.floor(difference / (1000 * 60 * 60 * 24));
+    const hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
+    const seconds = Math.floor((difference % (1000 * 60)) / 1000);
+    return days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
+  }
 
 }
