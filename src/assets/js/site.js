a51f87295a9dd4deabc186fffa61bec1edd4cc4f
dr33k <dr33k@users.noreply.github.com>
2023-09-21 03:14:11 +0100

    Added, styled image and made it responsive

--- src/assets/js/site.js
+++ src/assets/js/site.js
@@ -2,20 +2,22 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
-function update(counter){
-    var startDate = new Date("2021-01-01");
-    var difference = new Date() - startDate;
+$(document).ready(()=>{
+    $("#me-sm")
+    .hover(()=>{
+        $("#me-container").show()
+    },
+    ()=>{
+        $("#me-container").hide()
+    })
+    .click(()=>{
+        $("#me-container").show()
+    });
 
-    let days = Math.floor(difference / (1000 * 60 * 60 * 24));
-    let hours = Math.floor((difference % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    let minutes = Math.floor((difference % (1000 * 60 * 60)) / (1000 * 60));
-    let seconds = Math.floor((difference % (1000 * 60)) / 1000);
-    counter.innerHTML = days + " days " + hours + " hours " + minutes + " minutes " + seconds + " seconds";
-}
-
-function startCounter(){
-    var counter = document.getElementById("counter");
-    setInterval(()=>update(counter), 1000)
-}
-
-startCounter();
+    $("#me-lg").hover(()=>{
+        $("#me-container").show()
+    },
+    ()=>{
+        $("#me-container").hide()
+    });
+});
