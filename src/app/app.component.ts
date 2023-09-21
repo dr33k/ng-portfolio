b1c8acf317e7304581270104578afedd58a99b12
dr33k <dr33k@users.noreply.github.com>
2023-09-21 03:54:57 +0100

    Deployed

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { initializeApp } from "firebase/app";
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ng-portfolio';
+
+  firebaseConfig:any = {
+    apiKey: "AIzaSyAO-quiG-RMvt4w3RNnmXolG_d-sMUBTyQ",
+    authDomain: "fir-be-bdffc.firebaseapp.com",
+    projectId: "fir-be-bdffc",
+    storageBucket: "fir-be-bdffc.appspot.com",
+    messagingSenderId: "98725025062",
+  };
+
+  app = initializeApp(this.firebaseConfig);
+  
 }
