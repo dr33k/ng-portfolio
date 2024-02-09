7aa4433b9f0e3188be5e52de091de9e1b5f8b749
dr33k <dr33k@users.noreply.github.com>
2024-02-09 16:30:22 +0100

    Made qualifications page dynamic

--- src/app/home/qualifications/qualifications.component.ts
+++ src/app/home/qualifications/qualifications.component.ts
@@ -1,5 +1,7 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Type } from '@angular/core';
 import * as moment from 'moment';
+import { CertStruct, SkillStruct } from './qualifications.type';
+
 @Component({
   selector: 'app-qualifications',
   templateUrl: './qualifications.component.html',
@@ -12,12 +14,62 @@ import * as moment from 'moment';
 export class QualificationsComponent implements OnInit {
 
   counter: any;
-  skills: any = {
-    java: ['spring boot', 'spring cloud', 'quarkus', 'hibernate orm', 'junit5', 'mockito'],
-    ts: ['node js', 'express', 'angular', 'prisma orm', 'jest', 'supertest'],
-    python: ['vanilla python', 'django', 'chatbot'],
-    sql: ['postgresql', 'mysql']
-  }
+  skills: SkillStruct[] = [
+    {
+      name: 'java',
+      values: ['spring boot', 'spring cloud', 'quarkus', 'hibernate orm', 'junit5', 'mockito'],
+    },
+    {
+      name: 'python',
+      values: ['vanilla python', 'django', 'chatbot']
+    },
+    {
+      name: 'typescript',
+      values: ['node js', 'express', 'angular', 'prisma orm', 'jest', 'supertest']
+    },
+    {
+      name: 'sql',
+      values: ['postgresql', 'mysql']
+    },
+    {
+      name: 'nosql',
+      values: ['mongodb']
+    }
+  ]
+
+  certificates: CertStruct[] = [
+    {
+      name: 'Java',
+      authority: 'HiiT Plc',
+      completed: true,
+      link: 'https://drive.google.com/file/d/17ym5fgkXTI1A6F-tdj8Rciin73epSX2_/view?usp=sharing',
+    },
+    {
+      name: 'Microservices: Spring Boot and Spring Cloud',
+      authority: 'In28M',
+      completed: true,
+      link: 'https://drive.google.com/file/d/1vw7PgbAaxWkMeR-LF2MlwlGg8uSVoT-l/view?usp=sharing',
+    },
+    {
+      name: 'Node',
+      authority: 'The Hong Kong University Of Science and Technology',
+      completed: true,
+      link: 'https://drive.google.com/file/d/1k3GqZQzi3syAx_uTxkq8k-YzZ_8c-epz/view?usp=drive_link',
+    },
+    {
+      name: 'Angular',
+      authority: 'The Hong Kong University Of Science and Technology',
+      completed: true,
+      link: 'https://drive.google.com/file/d/1-SFTolBUT716AI9mqFB4V6Pltjr1qnhj/view?usp=drive_link',
+    },
+    {
+      name: 'Cybersecurity Professional',
+      authority: 'Google',
+      completed: false,
+    },
+  ]
+
+  resume_link = "https://drive.google.com/file/d/1_q2dBTL905teI3rReXC6HY-uZvI6eeBV/view?usp=sharing"
 
   constructor(){ this.counter=""}
 
---
+++ src/app/home/qualifications/qualifications.type.ts
@@ -0,0 +1,11 @@
+export interface CertStruct {
+    name: string;
+    authority: string;
+    completed: boolean;
+    link?: string;
+  }
+
+  export interface SkillStruct{
+    name: string;
+    values: string[];
+  }
